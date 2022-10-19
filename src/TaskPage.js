eed9056fa34e7b86a4fb88ba966342e0e54ca0a0
Robert Sainton <robertsainton@outlook.com>
2022-10-19 14:40:26 -0400

    imported Tasklist in Taskpage

--- src/TaskPage.js
+++ src/TaskPage.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import TaskList from "./TaskList";
 
 function TaskPage (){
 
     return(
+        <main>
+      <strong>Click X to add task to Delete page</strong>
+       <TaskList tasks={tasks} onMoveTask={onMoveTask}/>
+    </main>
 
     )
 }
