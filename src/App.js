759c3fb168cb97078cc4a2059b303da228719bfc
Robert Sainton <robertsainton@outlook.com>
2022-10-19 16:54:54 -0400

    added state to add tasks to delete page

--- src/App.js
+++ src/App.js
@@ -10,12 +10,31 @@ import DoneTasks from './DoneTasks';
 
 function App() {
    const [tasks , setTasks] = useState([])
+   const [trashTasks , setTrashTasks] = useState([])
 
    useEffect(()=>{
     fetch(" http://localhost:4001/tasks")
     .then((resp)=>resp.json())
     .then((tasks)=> setTasks(tasks))
   },[])
+  function handleAddtask(addedTask){
+    setTasks([...tasks, addedTask])
+  }
+  function handleMovetask(taskToMove){
+    console.log("line 29", taskToMove)
+    const taskToBeTrashed = trashTasks.find(
+      (task) => task.id === taskToMove.id
+    );
+    if (!taskToBeTrashed) {setTrashTasks([...trashTasks, taskToMove])}
+    console.log("line 34", trashTasks)
+
+    }
+    function handleDelete(deletedTask){
+      const updatedDisplay = tasks.filter((item) => item.id !== deletedTask.id)
+      setTasks(updatedDisplay)
+      const updatedTrashTask = trashTasks.filter((item)=> item.id !== deletedTask.id)
+      setTrashTasks(updatedTrashTask)
+    }
 
   return (
     <div >
