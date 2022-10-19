66c436afd0860c6ae37af81f593e27a71ac10fb2
Robert Sainton <robertsainton@outlook.com>
2022-10-19 14:55:30 -0400

    import taskcard in donetask

--- src/DoneTasks.js
+++ src/DoneTasks.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TaskCard from "./TaskCard";
 
 function DoneTasks(){
+    const tasksToBeDeletedList = tasks.map((item) => (
+        <TaskCard key={item.id} tasks={item} onTaskClick={onRemoveTask} />
+      ));
     return(
+        <div>
+            <strong>Click X to delete task forever</strong>
+            <ul className="cards">
+           {tasksToBeDeletedList}
+           </ul>
+        </div>
 
     )
 }
