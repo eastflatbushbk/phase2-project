dce4c76782bbed84939abaa9a965a4d83790cec8
Robert Sainton <robertsainton@outlook.com>
2022-10-19 14:33:56 -0400

    importing TaskCard to Tasklist

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,10 +1,16 @@
 import React from "react";
+import TaskCard from "./TaskCard";
 
-
-TaskList (){
-
+function TaskList (){
+    const displayCards = tasks.map((item)=>(
+        <TaskCard tasks={item} key={item.id} onTaskClick ={onMoveTask} />
+    ))
     return(
-        
+        <div>
+      <ul className="cards">
+      {displayCards}
+      </ul>
+    </div>
     )
 
 }
