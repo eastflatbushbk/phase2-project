ae67bb2efedae7259e4b2605f397556a7b22c76b
Robert Sainton <robertsainton@outlook.com>
2022-10-19 15:01:37 -0400

    added jsx in newtaskform component

--- src/NewTaskForm.js
+++ src/NewTaskForm.js
@@ -3,6 +3,19 @@
 function NewTaskForm (){
 
     return(
+        <div className="new-plant-form">
+        <h2>New Task</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="task" placeholder="Task ..." onChange={handleChange} value={formData.task}/>
+          <select name="marked"  onChange={handleChange} value={formData.marked} >
+            <option value= "default" hidden>Task marked as</option>
+            <option name="marked" value="least important">least important</option>
+            <option name="marked"value="IMPORTANT">IMPORTANT</option>
+            <option name="marked"value="URGENT!!">URGENT</option>
+           </select>
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
 
     )
 }
