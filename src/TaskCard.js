d5a82b2f3dd43c1c37560cf6e044f7fbc4d6a712
Robert Sainton <robertsainton@outlook.com>
2022-10-19 14:25:54 -0400

    created state for task card button

--- src/TaskCard.js
+++ src/TaskCard.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 function TaskCard (){
+    const [complete , setComplete] = useState(true)
+
+    function handleBtn (){
+        setComplete((complete)=>!complete)
+      }
 
     return(
-        
+        <li className="card">
+       <h4>{tasks.marked}</h4>
+        <p>{tasks.task}</p>
+        <div>
+         <button className="primary" onClick={handleBtn}>{complete ? "Click when complete":"completed"}</button>
+        <div>
+       <button onClick={handleClick}>X</button>
+       </div>
+      </div>
+    </li>
     )
 }
